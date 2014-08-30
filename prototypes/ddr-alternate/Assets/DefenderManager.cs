423d0daf5c85d34023427567761a97e8bfd33625
Julian Ceipek <julian.ceipek@gmail.com>
2014-08-30 16:49:42 -0700

    Added base movement inputs and sprite sheet

---
+++ prototypes/ddr-alternate/Assets/DefenderManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
