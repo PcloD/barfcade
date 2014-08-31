9eeaeab8e12b0f40ec1bd1a94aa725009b8fb4d4
Julian Ceipek <julian.ceipek@gmail.com>
2014-08-30 17:11:32 -0700

    Keep track of defender directions

--- prototypes/ddr-alternate/Assets/DefenderManager.cs
+++ prototypes/ddr-alternate/Assets/DefenderManager.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DefenderManager : MonoBehaviour {
 
+	public enum Phase {
+		Recording,
+		Duplicating
+	}
+
+	[System.Serializable]
+	public enum Dir {
+		Up,
+		Down,
+		Left,
+		Right
+	}
+
+	[SerializeField]
+	private List<Dir> directionQueue = new List<Dir>();
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		bool up = Input.GetButtonDown("P1_Up");
+		bool down = Input.GetButtonDown("P1_Down");
+		bool left = Input.GetButtonDown("P1_Left");
+		bool right = Input.GetButtonDown("P1_Right");
+		if (up) {
+			directionQueue.Add(Dir.Up);
+		}
+		if (down) {
+			directionQueue.Add(Dir.Down);
+		}
+		if (left) {
+			directionQueue.Add(Dir.Left);
+		}
+		if (right) {
+			directionQueue.Add(Dir.Right);
+		}
 	}
 }
